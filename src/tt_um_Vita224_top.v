e0b695a1268d38f0ef72955373931547f3e9a039
Vita224 <53021236+Vita224@users.noreply.github.com>
2024-06-05 17:08:46 -0600

    Update tt_um_Vita224_top.v

--- src/tt_um_Vita224_top.v
+++ src/tt_um_Vita224_top.v
@@ -20,11 +20,20 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module tt_um_Vita224_top(
+
+input  wire [7:0] ui_in,    // Dedicated inputs
+output wire [7:0] uo_out,   // Dedicated outputs
+input  wire [7:0] uio_in,   // IOs: Input path
+output wire [7:0] uio_out,  // IOs: Output path
+output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+input  wire       ena,      // will go high when the design is enabled
+input  wire       clk,      // clock
+input  wire       rst_n     // reset_n - low to reset
+  
 input [7:0]sw,
 input btn0,
 input btn1,
 input clk,
-input  wire       ena,      // always 1 when the design is powered, so you can ignore it
 output TxD,
 output TxD_debug,
 output transmit_debug,
