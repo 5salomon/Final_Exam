d74ed4d22aa807681bd1085fb18577896424bee2
Vita224 <53021236+Vita224@users.noreply.github.com>
2024-06-07 19:15:47 -0600

    Update tt_um_Vita224_top.v

--- src/tt_um_Vita224_top.v
+++ src/tt_um_Vita224_top.v
@@ -19,37 +19,47 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module tt_um_Vita224_top(
-
-input  wire [7:0] ui_in,    // Dedicated inputs
-output wire [7:0] uo_out,   // Dedicated outputs
-input  wire [7:0] uio_in,   // IOs: Input path
-output wire [7:0] uio_out,  // IOs: Output path
-output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-input  wire       ena,      // will go high when the design is enabled
-input  wire       clk,      // clock
-input  wire       rst_n,     // reset_n - low to reset
-  
+//module top(
 //input [7:0]sw,
 //input btn0,
 //input btn1,
 //input clk,
 //output TxD,
-//output TxD_debug,
-//output transmit_debug,
-//output button_debug, 
-//output clk_debug
-); 
+
+//); 
+module top(
+    input wire [7:0] ui_in, // Dedicated inputs
+    output wire [7:0] uo_out, // Dedicated outputs
+    input wire [7:0] uio_in, // IOs: Input path
+    output wire [7:0] uio_out, // IOs: Output path
+    output wire [7:0] uio_oe, // IOs: Enable path (active high: 0=input, 1=output)
+    input wire ena, // always 1 when the design is powered, so you can ignore it
+    input wire clk, // clock
+    input wire rst_n // reset_n - low to reset
+);
 
 wire transmit;
-assign TxD_debug = TxD;
-assign transmit_debug = transmit;
-assign button_debug = btn1;
-assign clk_debug = clk;
+//assign TxD_debug = tx;
+//assign transmit_debug = transmit;
+//assign button_debug = btn1;
+//assign clk_debug = clk;
+
+    wire tx;
+  assign uo_out[0]=1'b0;
+	assign uo_out[1]=1'b0;
+	assign uo_out[2]=1'b0;
+	assign uo_out[3]=1'b0;
+	assign uo_out[4]=tx;
+	assign uo_out[5]=1'b0;
+	assign uo_out[6]=1'b0;
+	assign uo_out[7]=1'b0;
+	
+assign uio_out = 8'b00000000;
+assign uio_oe = 8'b00000000;
 
 
-transmit_debouncing D2 (.clk(clk), .btn1(btn1), .transmit(transmit));
-transmitter T1 (.clk(clk), .reset(btn0),.transmit(transmit),.TxD(TxD),.data(sw));
+transmit_debouncing D2 (.clk(clk), .uio_in(uio_in), .transmit(transmit));
+transmitter T1 (.clk(clk), .rst_n(rst_n),.transmit(transmit),.tx(tx),.data(ui_in));
 
 
 endmodule
